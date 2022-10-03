f992da183ad834d1749f843920e2fed55b78d935
bglamadrid <bg.lamadrid@gmail.com>
2022-10-03 18:03:10 -0300

    feat: add form page with live submit button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RestapiModule } from 'src/restapi/restapi.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './routes/form/form.component';
-import { ResultsComponent } from './routes/results/results.component';
 import { NotfoundComponent } from './routes/notfound/notfound.component';
-import { RestapiModule } from 'src/restapi/restapi.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
+import { ResultsComponent } from './routes/results/results.component';
 
 @NgModule({
   declarations: [
@@ -23,9 +27,14 @@ import { MatMenuModule } from '@angular/material/menu';
     AppRoutingModule,
     RestapiModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
+    MatFormFieldModule,
     MatIconModule,
-    MatMenuModule
+    MatInputModule,
+    MatMenuModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routes/form/form.component.ts
+++ src/app/routes/form/form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
+import { delay, tap } from 'rxjs/operators';
+import { PollForm } from 'src/app/models/PollForm';
+import { MusicGenresService } from 'src/restapi/services/music-genres.service';
+import { PollAnswersService } from 'src/restapi/services/poll-answers.service';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  formGroup: FormGroup;
+  musicGenreNames$: Observable<string[]>;
+  submitting: Partial<Subscription> = { closed: true };
 
-  constructor() { }
+  constructor(
+    formBuilder: FormBuilder,
+    private musicGenresApi: MusicGenresService,
+    private pollAnswersApi: PollAnswersService
+  ) {
+    this.formGroup = formBuilder.group({
+      musicGenre: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])]
+    });
+    this.musicGenreNames$ = this.musicGenresApi.getMusicGenres();
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.submitting.closed) {
+      const form = this.formGroup.value as PollForm;
+      this.submitting = this.pollAnswersApi.postPollAnswer(form).pipe(
+        delay(1000),
+        tap({
+          next: () => {
+            alert('OK!');
+          },
+          error: () => {
+            alert('ERROR!');
+          }
+        })
+      ).subscribe();
+    }
   }
 
 }
