b0c46bd6b66b20f60266fb9b1067a3cd162babf5
bglamadrid <bg.lamadrid@gmail.com>
2022-10-03 15:04:07 -0300

    feat: add components & routes for each view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './routes/form/form.component';
+import { NotfoundComponent } from './routes/notfound/notfound.component';
+import { ResultsComponent } from './routes/results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'form',
+    component: FormComponent
+  },
+  {
+
+    path: 'results',
+    component: ResultsComponent
+  },
+  {
+    path: '',
+    component: FormComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './routes/form/form.component';
+import { ResultsComponent } from './routes/results/results.component';
+import { NotfoundComponent } from './routes/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ResultsComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
